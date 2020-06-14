57eac1a7aac8343d44f3f1f40c74e6ad6cbdf57e
cyitiaoxianyu0927 <3402399392@qq.com>
2020-06-14 14:24:52 +0800

    优化模板

--- src/page/page-vue/Index.vue
+++ src/page/page-vue/Index.vue
@@ -1,115 +1,28 @@
 <template>
- <div class="example">{{ msg }}
+  <div class="example">{{ msg }}
 
-    <span class="name">姓名：</span><el-input v-model="name" size="mini" style="width:160px" clearable></el-input> 
-    v12312
-vvkk
-    <i class="el-icon-delete"></i>1
-
-    <h4>这是个标题</h4>
-    <h4>这个是标题2</h4>
-    <h4>这个是标题2</h4>
-
-    <code-info :value="name"></code-info>
-
-    {{listData}}
   </div>
 </template>
 
 <script>
 
-//import codeInfo from "@/components/HelloWord.vue";
 
-import axios from "axios"
 
 export default {
   data () {
     return {
-      msg: 'Hello world!122',
-      name:"Jack",
-      listData:""
+      msg: '这个是VUE工程',
     }
   },
   components:{
      
-    //codeInfo 
-    codeInfo:()=>import("@/components/HelloWord.vue")
   },
   methods:{
-
-    // async queryList(){
- 
-       
-
-    //    let name = await setTimeout(()=>{
-
-    //      return "JACK"
-    //    })
-
-    //    console.log(name); 
- 
-    // } 
-
-    async queryList(){
-
-        await axios.post("http://localhost:5000/test/test1").then(res => {
-
-            this.listData = res;
-
-             console.log(res)
-        })   
-
-        console.log(12)
-
-
-    },
-    queryList2(){
-      
-        axios.post("http://localhost:5000/test/test2").then(res => {
-
-            this.listData = res;
-
-             console.log(res)
-        })   
-
-    },
-    queryList3(){
-      
-        axios.post("http://localhost:5000/test/test3").then(res => {
-
-            this.listData = res;
-
-             console.log(res)
-        })   
-
-    }
 
 
   },
   mounted(){
 
-    this.queryList();
-
-    this.queryList2();
-    
-    this.queryList3();
-
   }
 }
 </script>
-
-<style lang="less" scoped>
-
-  .example {
-    color: blue;
-    .name{
-
-       color:#fcaf41;
-       font-size: 12px;
-    }
-
-    .abc{
-        color:rgb(255,0,0)
-    }
-  }
-</style>
